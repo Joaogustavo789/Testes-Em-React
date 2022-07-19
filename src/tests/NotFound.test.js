cc95985a5fc71b235534e8791bfd90ebc1224ae6
Joao Gustavo <jgustavomendonca@gmail.com>
2022-07-19 18:33:24 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+test('Testa se a página contém um h2 com o texto Page requested not found 😭', () => {
+  renderWithRouter(<NotFound />);
+  const noFind = screen.getByText(/page requested not found/i);
+  expect(noFind).toBeInTheDocument();
+});
+
+test('Teste se a página mostra a imagem', () => {
+  renderWithRouter(<NotFound />);
+  const imageNotFound = screen.getByAltText(/pikachu crying because/i);
+  expect(imageNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
